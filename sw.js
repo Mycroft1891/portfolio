0f96691e93b78ccc18bf228435b55c9cde953c2a
Mycroft1891 <mycroft1891@gmail.com>
2018-03-16 03:10:45 +0100

    add index.html caching

--- sw.js
+++ sw.js
@@ -5,6 +5,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
+        'index.html'
         'css/custom.css',
         'img/jumbo-sm.jpg',
         'img/jumbo-md.jpg',
