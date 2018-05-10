4f596d4a3e1232a23a9a61ea67e11ed4a079ac16
Mycroft1891 <mycroft1891@gmail.com>
2018-05-10 23:01:08 +0200

    add UI logic

--- js/verse.js
+++ js/verse.js
@@ -1,8 +1,9 @@
 // bible verse selection logic
-
 var verse_paragraph = document.getElementById('verse');
+var verse_id = document.location.hash.split("=")[1];
 
 function get_day_of_year() {
+  if (typeof verse_id != 'undefined') return verse_id;
   var today = new Date();
   var beginning_of_year = new Date(today.getFullYear(), 0, 0);
   var time_zone_offset = beginning_of_year.getTimezoneOffset() - today.getTimezoneOffset()
@@ -12,6 +13,14 @@ function get_day_of_year() {
   return day_of_year
 }
 
+function get_verse(event) {
+  var language = event ? event.target.dataset.key : "english"
+  var day = get_day_of_year();
+  verse_paragraph.innerText = verses[day][language];
+}
+
 window.onload = function() {
-  verse_paragraph.innerText = get_day_of_year();
+  get_verse();
+  var language_list = document.querySelectorAll('.language');
+  language_list.forEach(lang => lang.addEventListener('click', get_verse));
 }
