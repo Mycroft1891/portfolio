3a0e6252f2d75cf463f555c938be60db4aa10cb1
Mycroft1891 <mycroft1891@gmail.com>
2018-05-22 21:31:47 +0200

    handle button click

--- js/teatime.js
+++ js/teatime.js
@@ -1,8 +1,11 @@
 var tea = {
+  "white": 60000,
   "green": 120000,
   "black": 240000,
   "infusion": 600000
 }
+
+let time_field = document.getElementById('time-field');
 
 function ms_in_minutes(ms) {
   let seconds = ms%60000;
@@ -12,7 +15,13 @@ function ms_in_minutes(ms) {
   return minutes + ":" + seconds
 }
 
+function changeTea(event) {
+  let type = this.dataset.key;
+  time_field.innerText = ms_in_minutes(tea[type]);
+}
+
 window.onload = function() {
-  let time_field = document.getElementById('time-field');
+  let tea_buttons = document.querySelectorAll('button');
+  tea_buttons.forEach(button => button.addEventListener("click", changeTea))
   time_field.innerText = ms_in_minutes(tea.green);
 }
