53a646f3396c6fa678d83d5c691f2e8a4ae5aa21
Mycroft1891 <mycroft1891@gmail.com>
2018-06-16 20:53:21 +0200

    improve syntax

--- js/verse.js
+++ js/verse.js
@@ -4,32 +4,32 @@ var book_paragraph = document.getElementById('verse_book');
 var verse_id = document.location.hash.split("=")[1];
 var base_url = "https://mycroft1891.github.io/portfolio/bible-verse.html";
 var verse = "";
-var book = ""
+var book = "";
 
 function get_day_of_year() {
   if (typeof verse_id != 'undefined') return verse_id;
   var today = new Date();
   var beginning_of_year = new Date(today.getFullYear(), 0, 0);
-  var time_zone_offset = beginning_of_year.getTimezoneOffset() - today.getTimezoneOffset()
+  var time_zone_offset = beginning_of_year.getTimezoneOffset() - today.getTimezoneOffset();
   var time_elapsed_in_ms = (today - beginning_of_year) + (time_zone_offset * 60 * 1000);
   var day = 1000 * 60 * 60 * 24;
   var day_of_year = Math.floor(time_elapsed_in_ms / day);
-  return day_of_year
+  return day_of_year;
 }
 
-function get_verse(event) {
-  var language = event ? event.target.dataset.key : "spanish"
+function get_verse(e) {
+  var language = e ? e.target.dataset.key : "spanish";
   var day = get_day_of_year();
-  verse_paragraph.innerText = verses[0][day][language]['verse'];
-  book_paragraph.innerText = verses[0][day][language]['book'];
   verse = verses[0][day][language]['verse'];
-  book = verses[0][day][language]['book']
+  book = verses[0][day][language]['book'];
+  verse_paragraph.innerText = verse;
+  book_paragraph.innerText = book;
   setupShareAction();
 }
 
 function setupShareAction() {
   var twitterButton = document.getElementById('twitter');
-  var twitter_url = "https://twitter.com/intent/tweet?text="
+  var twitter_url = "https://twitter.com/intent/tweet?text=";
   var tweet = encodeURI(verse + " " + book + " " + base_url);
   twitterButton.href = twitter_url + tweet;
 }
@@ -40,3 +40,5 @@ window.onload = function() {
   var language_list = document.querySelectorAll('.language');
   language_list.forEach(lang => lang.addEventListener('click', get_verse));
 }
+
+word   word   word    
